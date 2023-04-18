b56d9c7d30f52d76f239b7d360ecb404b4a21277
Tuuturuturuu <sofipost@ucm.es>
2023-04-18 18:49:54 +0200

    si

---
+++ CODCampamentos/src/Presentacion/Material/VMostrarTodosMateriales/VMostrarTodosMateriales.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package Presentacion.Material.VMostrarTodosMateriales;
+
+import javax.swing.JFrame;
+
+import Presentacion.Evento;
+import Presentacion.IGUI;
+import java.util.Set;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
+/** 
+* <!-- begin-UML-doc -->
+* <!-- end-UML-doc -->
+* @author airam
+* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+*/
+public class VMostrarTodosMateriales extends JFrame implements IGUI {
+	/** 
+	* <!-- begin-UML-doc -->
+	* <!-- end-UML-doc -->
+	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	private Set<JPanel> jPanel;
+	/** 
+	* <!-- begin-UML-doc -->
+	* <!-- end-UML-doc -->
+	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	private Set<JButton> jButton;
+	/** 
+	* <!-- begin-UML-doc -->
+	* <!-- end-UML-doc -->
+	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	private Set<JTextField> jTextField;
+	/** 
+	* <!-- begin-UML-doc -->
+	* <!-- end-UML-doc -->
+	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	private JFrame jFrame;
+	/** 
+	* <!-- begin-UML-doc -->
+	* <!-- end-UML-doc -->
+	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	private JLabel jLabel;
+
+	/** 
+	* (non-Javadoc)
+	* @see IGUI#actualizar()
+	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	public void actualizar() {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	@Override
+	public void actualizar(Object object, Evento event) {
+		// TODO Auto-generated method stub
+		
+	}
+}
